e1ff754dd05ad589b82ae9ab132d2d597af4093e
c-bata <contact@c-bata.link>
2015-11-04 19:52:43 +0900

    Rewrite js files using es6

--- static/gulpfile.babel.js
+++ static/gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp       from 'gulp';
 import browserify from 'browserify';
+import babelify   from 'babelify';
 import source     from 'vinyl-source-stream';
+import buffer     from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
 
 var paths = {
     test: "./test/*.js",
@@ -10,18 +13,22 @@ var paths = {
 
 
 gulp.task('browserify', () => {
-    browserify({entries: ["./js/app.js"]})
+    browserify({entries: [ paths.src + "app.js"]})
+        .transform(babelify)
         .bundle()
         .on('error', function(err) {
             console.log(err.message);
             this.emit('end');
         })
         .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.dist_src));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./js/**/*', ['browserify'])
+    gulp.watch( paths.src + '**/*', ['browserify'])
 });
 
 gulp.task('default', ['watch']);
--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,11 @@
-var angular = require('angular');
+import angular from 'angular';
+import { TodoController } from './TodoController';
 
-var app = angular.module('ToDo', []);
+let app = angular
+    .module('ToDo', [])
+    .controller('TodoController', TodoController);
 
 app.config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 });
 
-app.controller('TodoController', function($http){
-    var self = this;
-    self.todos = [];
-
-    self.getTodo = function() {
-        self.todos = $http.get('/api/tasks')
-            .success(function(data) {
-                self.todos = data['tasks'];
-            });
-    };
-
-    self.create = function() {
-        $http.post('/api/tasks', {'title': self.newTodo}).
-            success(function(data, status, headers, config){
-                console.log(data);
-                self.todos.push(data);
-            }).
-            error(function(data, status, headers, config){
-                console.log(data);
-            });
-
-        self.newTodo = '';
-    };
-
-    self.getTodo();
-});
